41b297b991ef37da16adf3675023af9fba17a42c
Тетяна Жульєва <128372881+TanyaJulieva@users.noreply.github.com>
2024-04-21 08:07:28 +0200

    pagin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,39 +2,45 @@ import { useEffect, useState } from "react";
 import { fetchImages } from "../images-api.js";
 import ImageGallery from "./ImageGallery/ImageGallery.jsx";
 import SearchBar from "./SearchBar/SearchBar.jsx";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn.jsx";
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
-  // useEffect(() => {
-  //   async function getImages() {
-  //     try {
-  //       setIsLoading(true);
-  //       const data = await fetchImages();
-  //       setImages(data);
-  //     } catch {
-  //       setError(true);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-
-  //   getImages();
-  // }, []);
-
-  const handleSearch = async (searchQuery) => {
-    try{
-      setIsLoading(true)
-      const data = await fetchImages(searchQuery);
-      setImages(data);
-    } catch{
-      setError(true)
-    } finally{
-      setIsLoading(false)
-    }
+  const handleSearch = async (newQuery) => {
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
   };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (query === "") {
+      return;
+    }
+
+    async function getImages() {
+      try {
+        setIsLoading(true);
+        const data = await fetchImages(query, page);
+        setImages((prevImages) => {
+          return [...prevImages, ...data];
+        });
+      } catch {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getImages();
+  }, [query, page]);
 
   return (
     <>
@@ -44,6 +50,8 @@ export default function App() {
       {images.length > 0 && <ImageGallery items={images} />}
 
       {isLoading && <b>Please wait</b>}
+
+      {images.length > 0 && !isLoading && <LoadMoreBtn onClick={handleLoadMore} />}
     </>
   );
 }
--- src/images-api.js
+++ src/images-api.js
@@ -4,11 +4,11 @@ const API_KEY = "BAIHqE3gLRBqivcNCSr9zD_QcvZuidt1nXlsnI7GNDg";
 
 axios.defaults.baseURL = "https://api.unsplash.com/photos/";
 
-export const fetchImages = async (searchQuery) => {
+export const fetchImages = async (searchQuery, currentPage) => {
   const response = await axios.get("", {
     params: {
       query: searchQuery,
-      page: 1,
+      page: currentPage,
       orientation: "landscape",
       client_id: API_KEY,
     },
