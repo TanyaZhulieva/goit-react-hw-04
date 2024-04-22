9737d21a2311e2e55a1fc812ede580175800c0f4
Тетяна Жульєва <128372881+TanyaJulieva@users.noreply.github.com>
2024-04-22 14:42:07 +0200

    upd

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,7 +6,7 @@ export default function ImageGallery({ items, onOpen }) {
     <ul className={css.gallery }>
       {items.map((item) => {
         return (
-          <li key={item.id} className={item}>
+          <li key={item.id} className={css.item}>
             <ImageCard 
             src={item.urls.small} alt={item.alt_description} openModal={()=>onOpen(item)}/>
           </li>
